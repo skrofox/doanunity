5396170ec9415adae74aec297acf9abf2d6c617b
skrofox <sangnguyendac825@gmail.com>
2025-12-07 03:47:57 +0700

    Enemy Setup

--- Assets/Scripts/Core/Entity.cs
+++ Assets/Scripts/Core/Entity.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-    public Animator anim { get; private set; }
     public Rigidbody2D rb;
+    public Animator anim { get; private set; }
     protected StateMachine stateMachine;
 
 
