49107350c0051cc0b77d7d55504a9377570ee7ef
skrofox <sangnguyendac825@gmail.com>
2025-12-09 21:47:28 +0700

    target detection.

---
+++ Assets/Scripts/Entity_Combat.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Entity_Combat : MonoBehaviour
+{
+
+    [Header("Taget Detection")]
+    [SerializeField] private Transform targetCheck;
+    [SerializeField] private float targetCheckRadius = 1;
+    [SerializeField] private LayerMask WhatIsTarget;
+
+
+    public void PerformAttack()
+    {
+        GetDetectedColliders();
+
+        foreach (var target in GetDetectedColliders())
+        {
+            Debug.Log("Attacking " + target.name);
+        }
+    }
+
+    private Collider2D[] GetDetectedColliders()
+    {
+        return Physics2D.OverlapCircleAll(targetCheck.position, targetCheckRadius, WhatIsTarget);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(targetCheck.position, targetCheckRadius);
+    }
+}
