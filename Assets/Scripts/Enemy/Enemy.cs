579320a3d8ca734c4d7f204a6e6d5b6243b1257b
skrofox <sangnguyendac825@gmail.com>
2025-12-10 15:27:42 +0700

    Player Death

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -35,6 +35,11 @@ public class Enemy : Entity
         stateMachine.ChangeState(deadState);
     }
 
+    private void HandlePlayerDeath()
+    {
+        stateMachine.ChangeState(idleState);
+    }
+
     public void TryEnterBattleState(Transform player)
     {
         if (stateMachine.currentState == battleState)
@@ -76,7 +81,16 @@ public class Enemy : Entity
         Gizmos.DrawLine(playerCheck.position, new Vector3(playerCheck.position.x + (facingDir * attackDistance), playerCheck.position.y));
         Gizmos.color = Color.green;
         Gizmos.DrawLine(playerCheck.position, new Vector3(playerCheck.position.x + (facingDir * minRetreatDistance), playerCheck.position.y));
+    }
 
+    private void OnEnable()
+    {
+        Player.OnPlayerDeath += HandlePlayerDeath;
+    }
+
+    private void OnDisable()
+    {
+        Player.OnPlayerDeath -= HandlePlayerDeath;
     }
 
 }
