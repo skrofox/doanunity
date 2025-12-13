6aa471303b726fca00fdb4017f32809f1ecb7a10
skrofox <sangnguyendac825@gmail.com>
2025-12-13 19:41:20 +0700

    Skill tree connection system

---
+++ Assets/Scripts/UI/UI_TreeConnectHandler.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class UI_TreeConnectDetails
+{
+    public UI_TreeConnectHandler childNode;
+    public NodeDirectionType direction;
+    [Range(100f, 350f)] public float length;
+}
+
+public class UI_TreeConnectHandler : MonoBehaviour
+{
+    private RectTransform rect;
+    [SerializeField] private UI_TreeConnectDetails[] connectionDetails;
+    [SerializeField] private UI_TreeConnection[] connections;
+
+    private void OnValidate()
+    {
+        if(rect == null)
+            rect = GetComponent<RectTransform>();
+
+        if (connectionDetails.Length != connections.Length)
+        {
+            Debug.Log("So luong chi tiet phai bang SL ket noi - " + gameObject.name);
+            return;
+        }
+
+        UpdateConnections();
+    }
+
+    private void UpdateConnections()
+    {
+        for (int i = 0; i < connectionDetails.Length; i++)
+        {
+            var detail = connectionDetails[i];
+            var connection = connections[i];
+            //connnected child node position = detail.GetConnnectionPoint
+            Vector2 targetPosition = connection.GetConnectionPoint(rect);
+
+            connection.DirectConnection(detail.direction, detail.length);
+            detail.childNode.SetPosition(targetPosition);
+        }
+    }
+
+    public void SetPosition(Vector2 position) => rect.anchoredPosition = position;
+}
